33c259995659ad50ecc5470cc79f32c9f6ddc573
GuillermoBaldan <guillermobaldan@gmail.com>
2022-09-09 16:08:37 +0200

    index.php updated! and style.css created

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php
-echo "works";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wellcome to login and register App</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <h1>Please Login or SignUp</h1>
+
+    <a href="login.php">Login</a> or 
+    <a href="signup.php">Signup</a>
+</body>
+</html>
