01f2e06a44716e14deabbab30b08ab31eeafd5b1
GuillermoBaldan <guillermobaldan@gmail.com>
2022-09-10 04:07:44 +0200

    header.php created

--- index.php
+++ index.php
@@ -1,17 +1,9 @@
 <?php include("db.php") ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP TODO APP</title>
-    <!-- version 5.2 of Bootstrap -->
-    <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-</head>
-<body>
+<?php include("includes/header.php") ?>
+<h1>Hello TODO APP!</h1>
+
+
 
 <!-- Scripts -->
 <!-- JavaScript Bundle with Popper -->
