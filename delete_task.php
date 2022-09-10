5984ca592f104a5c905034ba6b62efab5c7816b8
GuillermoBaldan <guillermobaldan@gmail.com>
2022-09-10 06:01:01 +0200

    edit function implemented

--- delete_task.php
+++ delete_task.php
@@ -3,7 +3,7 @@
 
  if(isset($_GET['id'])){
     $id = $_GET['id'];
-    $query = "DELETE FROM tasks WHERE id=$id";
+    $query = "DELETE FROM task WHERE id=$id";
     $result = mysqli_query($conn,$query);
     if (!$result){
         die("Query Failed");
