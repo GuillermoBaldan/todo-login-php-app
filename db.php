0d4b71833f325d136e0263149638efd37119a532
GuillermoBaldan <guillermobaldan@gmail.com>
2022-09-10 15:32:14 +0200

    update db conection

--- db.php
+++ db.php
@@ -1,18 +1,23 @@
 <?php
     if(!isset($_SESSION)) 
     { 
-        session_start(); 
+        session_start();
+        $server = 'localhost:3306';
+      $username = 'root';
+      $password = '';
+      $database = 'todo_login'; 
         $conn = mysqli_connect(
           'localhost',
           'root',
           '',
-          'todo_login'
+          $database
         ) or die(mysqli_erro($mysqli));
+        try {
+          $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
+        } catch (PDOException $error) {
+          die('Connection Failed: ' . $error->getMessage());
+        }
     } 
 
-    try {
-      $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
-    } catch (PDOException $error) {
-      die('Connection Failed: ' . $error->getMessage());
-    }
+
 ?>
