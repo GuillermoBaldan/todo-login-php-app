2feea5eb4d5c3702a379df8e21a4b006d0143e59
GuillermoBaldan <guillermobaldan@gmail.com>
2022-09-10 04:09:48 +0200

    footer.php created

--- index.php
+++ index.php
@@ -3,12 +3,6 @@
 <?php include("includes/header.php") ?>
 <h1>Hello TODO APP!</h1>
 
+<?php include("includes/footer.php") ?>
 
 
-<!-- Scripts -->
-<!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.6.1.slim.min.js" integrity="sha256-w8CvhFs7iHNVUtnSP0YKEg00p9Ih13rlL9zGqvLdePA=" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js" integrity="sha512-2rNj2KJ+D8s1ceNasTIex6z4HWyOnEYLVC3FigGOmyQCZc2eBXKgOxQmo3oKLHyfcj53uz4QMsRCWNbLd32Q1g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-</body>
-</html>
