0d36affb1b294f6d68a9be7c55b2ee61e8aadb26
GuillermoBaldan <guillermobaldan@gmail.com>
2022-09-10 04:13:12 +0200

    nav tag added to header.php

--- includes/header.php
+++ includes/header.php
@@ -10,3 +10,9 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
 <body>
+
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+        <a href="index.php" class="navbar-brand">TODO PHP CRUD</a>
+    <div>
+</nav>
