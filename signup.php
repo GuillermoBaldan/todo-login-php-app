24f9ceb5fa914816adf608999dd4fed07196c418
GuillermoBaldan <guillermobaldan@gmail.com>
2022-09-10 02:53:36 +0200

    connection and password encryption added

--- signup.php
+++ signup.php
@@ -1,5 +1,22 @@
 <?php
-    require  'database.php'
+
+  require 'database.php';
+
+  $message = '';
+
+  if (!empty($_POST['email']) && !empty($_POST['password'])) {
+    $sql = "INSERT INTO users (email, password) VALUES (:email, :password)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':email', $_POST['email']);
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+    $stmt->bindParam(':password', $password);
+
+    if ($stmt->execute()) {
+      $message = 'Successfully created new user';
+    } else {
+      $message = 'Sorry there must have been an issue creating your account';
+    }
+  }
 ?>
 
 <!DOCTYPE html>
@@ -14,6 +31,11 @@
 </head>
 <body>
    <?php require 'partials/header.php' ?>
+
+   <?php if(!empty($message)): ?>
+      <p> <?= $message ?></p>
+    <?php endif; ?>
+    
    <h1>SignUp</h1>
    <span> or <a href="login.php">Login</a></span>
    <form action="signup.php" method="POST">
